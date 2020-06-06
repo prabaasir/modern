a3af2430639a3577199d199957571692017a0bec
Shine <50831768+prabaasir@users.noreply.github.com>
2020-06-05 22:48:55 -0700

    Create main.js

---
+++ src/static/main.js
@@ -0,0 +1,24 @@
+//project-page
+$(document).ready(function () {
+  jQuery(function () {
+    jQuery('.show').click(function () {
+      jQuery('.targetDiv').hide();
+      jQuery('#div' + $(this).attr('target')).show();
+    });
+  });
+})
+
+$(document).ready(function () {
+  $('.dd .ad-link').click(function () {
+    $('.ad-link').removeClass("ad-active");
+    $(this).addClass("ad-active");
+  });
+});
+
+//service-page
+$(document).ready(function () {
+  $('.service-link .service-subtitle').click(function () {
+    $('.service-subtitle').removeClass('service-active')
+    $(this).addClass('service-active')
+  });
+});
